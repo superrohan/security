b04eaaf852b1b8324b8ce4371fb2c5fd5205ae8c
Rohan Bangera <162192148+superrohan@users.noreply.github.com>
2025-11-15 14:59:53 +0530

    Add RoleType enum for user roles

---
+++ RoleType.java
@@ -0,0 +1,10 @@
+package com.bank.capp.constants;
+
+public enum RoleType {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_SERVICE_RHAPP,
+    ROLE_SERVICE_ANALYTICS,
+    ROLE_SERVICE_SAPP,
+    ROLE_SERVICE_SCHEDULER
+}
