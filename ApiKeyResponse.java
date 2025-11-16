4e55cce9241ceadfa916ee02562d76cd2edb1457
Rohan Bangera <162192148+superrohan@users.noreply.github.com>
2025-11-16 12:57:00 +0530

    Create ApiKeyResponse class for API key responses

---
+++ ApiKeyResponse.java
@@ -0,0 +1,19 @@
+package com.bank.capp.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiKeyResponse {
+    
+    private String apiKey;
+    private String serviceName;
+    private String message;
+}
