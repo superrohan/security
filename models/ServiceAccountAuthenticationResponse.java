09d4be5051fb4c04718d9bf05c2eebd6a54abd6c
Rohan Bangera <162192148+superrohan@users.noreply.github.com>
2025-12-15 20:25:16 +0530

    Add ServiceAccountAuthenticationResponse model

---
+++ models/ServiceAccountAuthenticationResponse.java
@@ -0,0 +1,29 @@
+package com.bank.capp.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceAccountAuthenticationResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("expires_in")
+    private Long expiresIn;
+
+    @JsonProperty("service_name")
+    private String serviceName;
+}
